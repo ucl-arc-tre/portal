1f8446cbe1b1beb4bf958c2f3b0b4b115e282f33
t-young31 <tom.young.316@gmail.com>
2025-04-09 13:01:53 +0100

    add non auditable models

--- internal/types/main.go
+++ internal/types/main.go
@@ -10,6 +10,10 @@ import (
 type Model struct {
 	ID        uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4()"`
 	CreatedAt time.Time
+}
+
+type ModelAutitable struct {
+	Model
 	UpdatedAt time.Time
 	DeletedAt gorm.DeletedAt `gorm:"index"`
 }
