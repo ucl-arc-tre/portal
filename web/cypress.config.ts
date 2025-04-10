afe27abe8642a777dd0b42547e855707915984c0
James Hughes <james.hughes@ucl.ac.uk>
2025-04-10 13:12:18 +0100

    converted cypress files to ts

--- web/cypress.config.ts
+++ web/cypress.config.ts
@@ -1,6 +1,6 @@
-const { defineConfig } = require("cypress");
+import { defineConfig } from "cypress";
 
-module.exports = defineConfig({
+export default defineConfig({
   e2e: {
     baseUrl: process.env.CYPRESS_baseUrl || "http://localhost:5173",
     supportFile: false,
--- web/eslint.config.js
+++ web/eslint.config.js
@@ -13,7 +13,7 @@ export default tseslint.config(
       ecmaVersion: 2020,
       globals: globals.browser,
       parserOptions: {
-        project: ["./tsconfig.node.json", "./tsconfig.app.json"],
+        project: ["./tsconfig.node.json", "./tsconfig.app.json", "./tsconfig.json"],
         tsconfigRootDir: import.meta.dirname,
       },
     },
