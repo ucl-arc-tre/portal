c3d08d77000c4273316e153ff9a6e9840d48d9a9
acholyn <a.ho-lyn@ucl.ac.uk>
2025-04-16 12:03:54 +0100

    tweaking cypress title test to get by id

--- web/cypress/e2e/app.cy.ts
+++ web/cypress/e2e/app.cy.ts
@@ -2,7 +2,7 @@ describe("ARC Portal UI", () => {
   it("shows the ARC portal phrase on the homepage", () => {
     cy.visit("/");
 
-    cy.contains("ARC portal").should("be.visible");
+    cy.get("#portal-title").should("be.visible");
     cy.contains("fibble").should("not.exist");
   });
 });
