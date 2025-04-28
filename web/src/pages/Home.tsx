9821c538be07762adaf3084af1e1f70304bf898a
acholyn <a.ho-lyn@ucl.ac.uk>
2025-04-28 10:29:25 +0100

    removing UIkit comments + refs

--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -1,15 +1,9 @@
-// import Button from "../../pkgs/uikit/lib/components/Button";
-
 export default function Home() {
   return (
     <div>
       <button>projects</button>
       <button>studies</button>
       <button>assets</button>
-      {/* TODO: UIKIT
-      <Button>Projects</Button>
-      <Button>Studies</Button>
-      <Button>Assets</Button> */}
     </div>
   );
 }
--- web/src/pages/Login.tsx
+++ web/src/pages/Login.tsx
@@ -1,9 +1,6 @@
-// import Button from "../../pkgs/uikit/lib/components/Button";
-
 export default function Login() {
   return (
     <div>
-      {/* TODO: UIKIT */}
       <button>
         <a href="/oauth2/start">Login with UCL SSO</a>
       </button>
