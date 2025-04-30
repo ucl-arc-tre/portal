5cabd5c618bf481b086554795c3ca0ec36c0b87c
acholyn <a.ho-lyn@ucl.ac.uk>
2025-04-30 14:26:48 +0100

    moving content to main page

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,64 +1,51 @@
-import Image from "next/image";
+"use client";
+
+import { useEffect, useState } from "react";
+
+import { getProfile } from "../openapi";
 import styles from "./page.module.css";
 
 export default function Home() {
+  const [helloMessage, setHelloMessage] = useState<string>("");
+  const [isAuthed, setIsAuthed] = useState<boolean | undefined>(undefined);
+
+  useEffect(() => {
+    getProfile()
+      .then((res) => {
+        if (res.response.status == 200 && res.data) {
+          setHelloMessage(`Username: ${res.data.username}. Roles: ${res.data.roles.join(",")}`);
+          setIsAuthed(true);
+        } else {
+          setIsAuthed(false);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
   return (
     <div className={styles.page}>
-      <main className={styles.main}>
-        <Image className={styles.logo} src="/next.svg" alt="Next.js logo" width={180} height={38} priority />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+      <h1 id="portal-title">Welcome to the ARC Services Portal</h1>
+      <main aria-label="ARC portal main content">
+        {isAuthed === false && (
+          <div>
+            <button>
+              <a href="/oauth2/start">Login with UCL SSO</a>
+            </button>
+          </div>
+        )}
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image className={styles.logo} src="/vercel.svg" alt="Vercel logomark" width={20} height={20} />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
+        {isAuthed === true && (
+          <>
+            {/* TODO: get rid of this at some point - replace with some sort of notif on login */}
+            <div className="card">
+              <p>
+                GET /profile → <strong>{helloMessage}</strong>
+              </p>
+            </div>
+          </>
+        )}
       </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image aria-hidden src="/file.svg" alt="File icon" width={16} height={16} />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image aria-hidden src="/window.svg" alt="Window icon" width={16} height={16} />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image aria-hidden src="/globe.svg" alt="Globe icon" width={16} height={16} />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
