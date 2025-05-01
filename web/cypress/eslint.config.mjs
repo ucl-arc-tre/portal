0d07b59cdaacabbfd67ab211f9166bb081261d81
acholyn <a.ho-lyn@ucl.ac.uk>
2025-05-01 17:02:39 +0100

    trying to fix cypress linting

--- web/cypress/eslint.config.mjs
+++ web/cypress/eslint.config.mjs
@@ -1,17 +1,30 @@
 import path from "path";
+
 import { fileURLToPath } from "url";
+
+import { FlatCompat } from "@eslint/eslintrc";
+import { fixupConfigRules } from "@eslint/compat";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const cypressConfig = {
-  extends: path.join(__dirname, "..", ".eslintrc.mjs"),
+const mainEslintConfig = path.join(__dirname, "..", "eslint.config.mjs");
 
-  rules: {
-    "@typescript-eslint/no-require-imports": "off",
-    "@typescript-eslint/no-unsafe-assignment": "off",
-    "@typescript-eslint/no-unsafe-call": "off",
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const patchedConfig = fixupConfigRules([...compat.extends(mainEslintConfig)]);
+
+const cypressConfig = [
+  ...patchedConfig,
+  {
+    rules: {
+      "@typescript-eslint/no-require-imports": "off",
+      "@typescript-eslint/no-unsafe-assignment": "off",
+      "@typescript-eslint/no-unsafe-call": "off",
+    },
   },
-};
+];
 
 export default cypressConfig;
