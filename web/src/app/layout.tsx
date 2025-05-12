9f94633c1f921bb16b14162245fffeb50217073f
James Hughes <james.hughes@ucl.ac.uk>
2025-05-12 17:50:54 +0100

    removed unneeded openAPI setup

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -4,8 +4,6 @@ import "./layout.css";
 import Nav from "@/components/nav/Nav";
 import type { ReactNode } from "react";
 
-import "@/lib/setupOpenApi";
-
 export const metadata = {
   title: "UCL ARC | portal",
   description: "Portal for UCL ARC services",
--- web/src/lib/setupOpenApi.ts
+++
@@ -1,3 +0,0 @@
-import { client } from "@/openapi/client.gen";
-
-client.setConfig({ baseUrl: "/api/v0" });
