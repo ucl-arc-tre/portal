66bf64f8bc0041a3ffa7da88edd67aa33ef17c67
James Hughes <james.hughes@ucl.ac.uk>
2025-05-12 09:51:37 +0100

    now using traditional function syntax

--- web/src/app/not-found.tsx
+++ web/src/app/not-found.tsx
@@ -1,12 +1,10 @@
 import Link from "next/link";
 
-const NotFound = () => {
+export default function NotFound() {
   return (
     <div>
       <p>Sorry, the requested page does not exist.</p>
       <Link href="/">Back to Home</Link>
     </div>
   );
-};
-
-export default NotFound;
+}
