b005fe596f54e0d98afa20a97a6dc6d57a90c954
t-young31 <tom.young.316@gmail.com>
2025-05-16 10:46:34 +0100

    fix attr name

--- web/src/app/profile/approved-researcher/components/AgreementText.tsx
+++ web/src/app/profile/approved-researcher/components/AgreementText.tsx
@@ -4,13 +4,13 @@ import "./AgreementText.css";
 import Markdown from "react-markdown";
 
 type AgreementTextProps = {
-  agreementText: string;
+  text: string;
 };
 
 export default function AgreementText(props: AgreementTextProps) {
   return (
     <section className="section">
-      <Markdown>{props.agreementText}</Markdown>
+      <Markdown>{props.text}</Markdown>
     </section>
   );
 }
