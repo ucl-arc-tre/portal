faa56dfd710cec8076bca2edc7866763059f7d15
acholyn <a.ho-lyn@ucl.ac.uk>
2025-05-27 15:05:36 +0100

    adding to layout

--- web/src/components/layout/Layout.tsx
+++ web/src/components/layout/Layout.tsx
@@ -1,3 +1,4 @@
+// import { Footer, Header } from "uikit-react-public";
 import styles from "./Layout.module.css";
 import Nav from "@/components/nav/Nav";
 
@@ -5,8 +6,10 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <div className={styles.layout}>
+        {/* <Header title="UCL ARC Services Portal" /> */}
         <Nav />
         <main className={styles.main}>{children}</main>
+        {/* <Footer /> */}
       </div>
     </>
   );
