440371594d05e985c39451321737b9e334a9316c
acholyn <a.ho-lyn@ucl.ac.uk>
2025-05-29 14:52:50 +0100

    improved ucl header

--- web/src/components/layout/Layout.tsx
+++ web/src/components/layout/Layout.tsx
@@ -12,7 +12,7 @@ const Footer = dynamic(() => import("uikit-react-public").then((mod) => mod.Foot
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <Header title="UCL ARC Services Portal" />
+      <Header title="UCL ARC Services Portal" className={styles.ucl_header} />
       <Nav />
       <div className={styles.layout}>
         <main className={styles.content}>{children}</main>
