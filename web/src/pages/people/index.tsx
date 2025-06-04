677304729600e6ce630d9e98d6e2a1ab0d49ce60
acholyn <a.ho-lyn@ucl.ac.uk>
2025-06-04 11:56:11 +0100

    basics for admin people view

---
+++ web/src/pages/people/index.tsx
@@ -0,0 +1,33 @@
+import MetaHead from "@/components/meta/Head";
+import AdminView from "@/components/people/AdminView";
+import LoginFallback from "@/components/ui/LoginFallback";
+import { useAuth } from "@/hooks/useAuth";
+
+export default function PeoplePage() {
+  const { loading, isAuthed, userData } = useAuth();
+  if (loading) return null;
+
+  if (!isAuthed) return <LoginFallback />;
+
+  console.log(userData);
+
+  const cannotView = !userData?.roles.includes("admin") && !userData?.roles.includes("approved_researcher");
+
+  console.log(cannotView);
+
+  return (
+    <>
+      <MetaHead
+        title="People | ARC Services Portal"
+        description="View and modify people you're permitted to manage in the ARC Services Portal"
+      />
+      <h1>People</h1>
+      {cannotView && <h4>You do not have permission to view this page</h4>}
+      {userData?.roles.includes("admin") ? (
+        <AdminView />
+      ) : (
+        userData?.roles.includes("approved_researcher") && <p>Approved Researcher</p>
+      )}
+    </>
+  );
+}
