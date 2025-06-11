e8e5445289d1053154af2de98eb6dfb3000a1968
acholyn <a.ho-lyn@ucl.ac.uk>
2025-06-11 10:35:11 +0100

    expanding type + adding style + tidying

--- web/src/components/people/AdminView.tsx
+++ web/src/components/people/AdminView.tsx
@@ -1,15 +1,28 @@
 import { getPeople, PeopleAdminResponse, PeopleApprovedResearcherResponse } from "@/openapi";
 import { useEffect, useState } from "react";
+import styles from "./AdminView.module.css";
+import dynamic from "next/dynamic";
 
+type Agreement = {
+  agreement_type: string;
+  confirmed_at: string;
+};
 type PersonAdminView = {
   User: {
     ID: string;
     CreatedAt: string;
     Username: string;
   };
-  Agreements: [];
+  Agreements: Agreement[];
   Roles: string[];
 };
+
+const CheckIcon = dynamic(() => import("uikit-react-public").then((mod) => mod.Icon.Check), {
+  ssr: false,
+});
+const XIcon = dynamic(() => import("uikit-react-public").then((mod) => mod.Icon.X), {
+  ssr: false,
+});
 
 function isAdminResponse(data: PeopleAdminResponse | PeopleApprovedResearcherResponse): data is PeopleAdminResponse {
   return Array.isArray(data) && data.every((item) => "User" in item && "Agreements" in item);
@@ -35,10 +48,10 @@ export default function AdminView() {
     fetchPeople();
   }, []);
 
-  console.log("people", people);
   if (!people) return null;
+
   return (
-    <table>
+    <table className={styles.table}>
       <thead>
         <tr>
           <th>User</th>
@@ -49,11 +62,18 @@ export default function AdminView() {
       <tbody>
         {people!.map((person: PersonAdminView) => (
           <tr key={person.User.ID}>
-            <td>
+            <td className={styles.user}>
               {person.User.Username} <small>{person.User.ID}</small>
             </td>
-            <td>{person.Roles}</td>
-            <td>{person.Agreements}</td>
+            <td className={styles.roles}>{person.Roles}</td>
+            <td className={styles.agreements}>
+              {Object.entries(person.Agreements).map(([key, agreement]) => (
+                <div key={key}>
+                  {agreement!.agreement_type}
+                  {agreement!.confirmed_at! ? <CheckIcon /> : <XIcon />}
+                </div>
+              ))}
+            </td>
           </tr>
         ))}
       </tbody>
