7992911758cffbd425e5dc109919d27f08531c41
James Hughes <james.hughes@ucl.ac.uk>
2025-06-19 16:10:11 +0100

    removed redundant test line

--- web/cypress/e2e/app.cy.ts
+++ web/cypress/e2e/app.cy.ts
@@ -35,7 +35,6 @@ describe("ARC Portal UI authenticated", () => {
     cy.loginAsBase();
 
     cy.visit("/");
-    cy.contains("Loading...").should("not.exist");
     cy.contains("Username").should("exist");
   });
 
