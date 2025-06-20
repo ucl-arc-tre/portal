8e57dec51a5f3afd2e8109ca6375976badf599ed
James Hughes <70143648+jhughes982@users.noreply.github.com>
2025-06-20 16:11:04 +0100

    refactored accessibility tests (#126)
    * refactored accessibility tests which are now failing
    
    * fixing accessibility test failure
    
    * fixing another accessibility test

--- web/cypress/e2e/accessibility.cy.ts
+++ web/cypress/e2e/accessibility.cy.ts
@@ -6,15 +6,53 @@ const pages = [
   { name: "Studies page", path: "/studies" },
   { name: "Assets page", path: "/assets" },
   { name: "Projects page", path: "/projects" },
+  { name: "People page", path: "/people" },
 ];
 
 describe("Accessibility - ARC Portal", () => {
-  pages.forEach(({ name, path }) => {
-    it(`should have no critical or serious accessibility violations on ${name}`, () => {
-      cy.visit(path);
-      cy.injectAxe();
-      cy.checkA11y(undefined, {
-        includedImpacts: ["critical", "serious"],
+  describe("Unauthenticated content", () => {
+    pages.forEach(({ name, path }) => {
+      it(`should have no critical accessibility violations on ${name} (login fallback)`, () => {
+        cy.visit(path);
+        cy.get("header").should("be.visible"); // wait for page layout to load
+        cy.injectAxe();
+        cy.checkA11y(undefined, {
+          includedImpacts: ["critical", "serious"],
+        });
+      });
+    });
+  });
+
+  describe("Base user content", () => {
+    beforeEach(() => {
+      cy.loginAsBase();
+    });
+
+    pages.forEach(({ name, path }) => {
+      it(`should have no critical accessibility violations on ${name} (base user)`, () => {
+        cy.visit(path);
+        cy.get("header").should("be.visible"); // wait for page layout to load
+        cy.injectAxe();
+        cy.checkA11y(undefined, {
+          includedImpacts: ["critical", "serious"],
+        });
+      });
+    });
+  });
+
+  describe("Admin user content", () => {
+    beforeEach(() => {
+      cy.loginAsAdmin();
+    });
+
+    pages.forEach(({ name, path }) => {
+      it(`should have no critical accessibility violations on ${name} (admin user)`, () => {
+        cy.visit(path);
+        cy.get("header").should("be.visible"); // wait for page layout to load
+        cy.injectAxe();
+        cy.checkA11y(undefined, {
+          includedImpacts: ["critical", "serious"],
+        });
       });
     });
   });
