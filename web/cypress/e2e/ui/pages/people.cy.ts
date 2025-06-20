da907b85af21e7813bcac957bf309b88fb7cdfaa
James Hughes <james.hughes@ucl.ac.uk>
2025-06-20 11:24:32 +0100

    attempting test restructure

---
+++ web/cypress/e2e/ui/pages/people.cy.ts
@@ -0,0 +1,34 @@
+beforeEach(() => {
+  cy.clearCookies();
+  cy.clearLocalStorage();
+});
+
+describe("People page content", () => {
+  it("should show nothing to base role", () => {
+    cy.loginAsBase();
+    cy.mockAuthAsBaseUser();
+    cy.visit("/people");
+    cy.waitForMockedAuth();
+
+    cy.contains("You do not have permission to view this page").should("be.visible");
+  });
+
+  it("should show content for base approved researcher", () => {
+    cy.loginAsBase();
+    cy.mockAuthAsBaseApprovedResearcher();
+    cy.visit("/people");
+    cy.waitForMockedAuth();
+
+    cy.contains("You do not have permission to view this page").should("not.exist");
+    cy.contains("Approved Researcher").should("be.visible");
+  });
+
+  it("should show content for admin", () => {
+    cy.loginAsAdmin();
+    cy.visit("/people");
+
+    cy.contains("You do not have permission to view this page").should("not.exist");
+    cy.get("table").contains("User").should("be.visible");
+    cy.contains(Cypress.env("botAdminUsername")).should("be.visible");
+  });
+});
