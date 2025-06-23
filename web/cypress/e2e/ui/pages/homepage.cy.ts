be6035dcae70eb5eff9802719d26498dcbf34eb8
James Hughes <james.hughes@ucl.ac.uk>
2025-06-23 11:23:04 +0100

    moved test to new file

--- web/cypress/e2e/ui/pages/homepage.cy.ts
+++ web/cypress/e2e/ui/pages/homepage.cy.ts
@@ -20,3 +20,46 @@ describe("ARC Portal UI authenticated", () => {
     cy.contains("Username").should("exist");
   });
 });
+
+describe("Setting chosen name for user", () => {
+  beforeEach(() => {
+    cy.loginAsBase();
+    cy.clearChosenName();
+    cy.visit("/");
+  });
+
+  const chosenName = (Cypress.env("chosenName") as string) || "Test Chosen Name";
+
+  it("prompts user to set chosen name", () => {
+    cy.get("dialog[data-cy='chosenName']").should("be.visible");
+  });
+
+  it("can submit chosen name form", () => {
+    // Mock the API call
+    cy.intercept("POST", "/api/v0/profile", {
+      statusCode: 200,
+    }).as("saveProfile");
+
+    cy.get("dialog[data-cy='chosenName']").find("input").type(chosenName);
+    cy.get("dialog[data-cy='chosenName']").find("button").click();
+
+    // Verify API was called correctly
+    cy.wait("@saveProfile").then((interception) => {
+      expect(interception.request.body).to.have.property("chosen_name", chosenName);
+    });
+
+    // Verify dialog closes
+    cy.get("dialog[data-cy='chosenName']").should("not.exist");
+  });
+
+  it("can't set invalid name", () => {
+    cy.get("dialog[data-cy='chosenName']").within(() => {
+      cy.get("input").type("123533");
+      cy.get("button").click();
+      cy.get("[data-testid='ucl-uikit-alert']").should("be.visible").and("contain", "Please enter a valid name");
+    });
+
+    // Make sure the dialog is still open
+    cy.get("dialog[data-cy='chosenName']").should("be.visible");
+  });
+});
