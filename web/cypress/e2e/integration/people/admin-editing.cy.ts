212555c4a12f7c44e99be31bb39af8b48775ea24
acholyn <a.ho-lyn@ucl.ac.uk>
2025-06-25 13:10:41 +0100

    fixing tests

--- web/cypress/e2e/integration/people/admin-editing.cy.ts
+++ web/cypress/e2e/integration/people/admin-editing.cy.ts
@@ -1,22 +1,11 @@
 import { botBaseUsername } from "../../../support/commands";
 
-beforeEach(() => {
-  cy.clearCookies();
-  cy.clearLocalStorage();
-});
-
 describe("Admin can edit people", () => {
   beforeEach(() => {
+    cy.clearCookies();
+    cy.clearLocalStorage();
     cy.loginAsAdmin();
     cy.visit("/people");
-  });
-
-  it("can edit a person's username", () => {
-    cy.contains("tr", botBaseUsername).find("[data-cy='username']").contains("Edit").click();
-
-    cy.get("input[name='username']").type("basebot@tre.com");
-    cy.get("button").contains("Submit").click();
-    cy.contains("basebot@tre.com").should("be.visible");
   });
 
   it("can edit a person's training record", () => {
@@ -25,5 +14,13 @@ describe("Admin can edit people", () => {
     cy.get("select[name='training_kind']").select("nhsd");
     cy.get("button").contains("Submit").click();
     cy.contains("tr", botBaseUsername).find("[data-cy='training']").contains("nhsd").should("be.visible");
+  });
+
+  it("can edit a person's username", () => {
+    cy.contains("tr", botBaseUsername).find("[data-cy='username']").contains("Edit").click();
+
+    cy.get("input[name='username']").type("basebot@tre.com");
+    cy.get("button").contains("Submit").click();
+    cy.contains("basebot@tre.com").should("be.visible");
   });
 });
