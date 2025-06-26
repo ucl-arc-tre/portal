ce88a800e9d1c3dddba8a15be7aa048269cd1b7a
acholyn <a.ho-lyn@ucl.ac.uk>
2025-06-26 15:08:53 +0100

    fixing test

--- web/cypress/e2e/integration/people/admin-editing.cy.ts
+++ web/cypress/e2e/integration/people/admin-editing.cy.ts
@@ -12,7 +12,7 @@ describe("Admin can edit people", () => {
     cy.contains("tr", botBaseUsername).find("[data-cy='training']").contains("Edit").click();
 
     cy.get("select[name='training_kind']").select("nhsd");
-    cy.get("set-to-today").click();
+    cy.get("[data-cy=set-to-today]").click();
     cy.get("[name='training_date']").should("not.be.visible");
     cy.get("button").contains("Submit").click();
     cy.contains("tr", botBaseUsername).find("[data-cy='training']").contains("nhsd").should("be.visible");
