83e38bdef9ea4874349b744d8b171ccd087a0662
acholyn <a.ho-lyn@ucl.ac.uk>
2025-07-02 11:08:25 +0100

    removing explicit admin role check

--- internal/handler/web/people.go
+++ internal/handler/web/people.go
@@ -36,43 +36,30 @@ func (h *Handler) GetPeople(ctx *gin.Context) {
 }
 
 func (h *Handler) PostPeopleId(ctx *gin.Context, id string) {
-	user := middleware.GetUser(ctx)
-	roles, err := rbac.GetRoles(user)
-	if err != nil {
-		setServerError(ctx, err, "Failed to get roles for user")
+	var update openapi.PersonUpdate
+	if err := ctx.ShouldBindJSON(&update); err != nil {
+		setInvalid(ctx, err, "Invalid JSON object")
 		return
 	}
 
-	if slices.Contains(roles, "admin") {
-		var update openapi.PersonUpdate
-		if err := ctx.ShouldBindJSON(&update); err != nil {
-			setInvalid(ctx, err, "Invalid JSON object")
-			return
+	// take the id from the url and get the person
+	person, err := h.users.GetPerson(id)
+	if err != nil {
+		setServerError(ctx, err, "Failed to get person")
+		return
+	}
+
+	if update.TrainingKind != nil {
+		date := *update.TrainingDate
+
+		if err := h.users.SetTrainingValidity(person, types.TrainingKind(*update.TrainingKind), (date)); err != nil {
+			setServerError(ctx, err, "Failed to update training validity")
 		}
-
-		// take the id from the url and get the person
-		id := ctx.Params.ByName("id")
-		person, err := h.users.GetPerson(id)
-		if err != nil {
-			setServerError(ctx, err, "Failed to get person")
-			return
-		}
-
-		if update.TrainingKind != nil {
-			date := *update.TrainingDate
-
-			if err := h.users.SetTrainingValidity(person, types.TrainingKind(*update.TrainingKind), (date)); err != nil {
-				setServerError(ctx, err, "Failed to update training validity")
-			}
-			ctx.JSON(http.StatusOK, openapi.TrainingRecord{
-				Kind:        *update.TrainingKind,
-				CompletedAt: &date,
-				IsValid:     true,
-			})
-		}
-
-	} else {
-		ctx.JSON(http.StatusForbidden, gin.H{"error": "Forbidden"})
+		ctx.JSON(http.StatusOK, openapi.TrainingRecord{
+			Kind:        *update.TrainingKind,
+			CompletedAt: &date,
+			IsValid:     true,
+		})
 	}
 
 }
