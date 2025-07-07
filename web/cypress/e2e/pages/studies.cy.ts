265f307afe122cd1c01a9703f6fb9d033541e8d0
acholyn <a.ho-lyn@ucl.ac.uk>
2025-07-07 16:40:07 +0100

    more for tests

--- web/cypress/e2e/pages/studies.cy.ts
+++ web/cypress/e2e/pages/studies.cy.ts
@@ -37,17 +37,34 @@ describe("Approved researcher can create studies", () => {
     cy.get("[data-cy='create-study-form']").should("be.visible");
   });
 
-  it("can't edit study owner", () => {
+  it("owner set to user & can't be edited", () => {
     cy.fixture("auth-base-approved-researcher.json").then((data) => {
       const username = data.username;
 
       cy.contains("Create Study").should("be.visible").click();
 
-      cy.get("input[name='owner']").should("be.readonly").should("have.value", username);
+      cy.get("input[name='owner']").should("have.attr", "readonly");
+      cy.get("input[name='owner']").should("have.value", username);
     });
   });
 
   it("can't submit without filling in required fields", () => {
     cy.contains("Create Study").should("be.visible").click();
+
+    // click through form
+    cy.get("[data-cy='next']").click();
+    cy.get("[data-cy='next']").click();
+    cy.get("button[type='submit']").should("be.disabled");
+
+    // go back to start of form and fill in the fields
+    cy.get("[data-cy='back']").click();
+    cy.get("[data-cy='back']").click();
+    cy.get("input[name='shortStudyName']").type("Test Study");
+    cy.get("input[name='controller']").type("Test Controller");
+
+    // go to the end and submit
+    cy.get("[data-cy='next']").click();
+    cy.get("[data-cy='next']").click();
+    cy.get("button[type='submit']").should("not.be.disabled");
   });
 });
