d93a68d38df3e17229202f31770ec4bf6fb5b0f5
acholyn <a.ho-lyn@ucl.ac.uk>
2025-07-08 09:22:20 +0100

    basic tooltips for project and asstes

--- web/src/pages/studies/index.tsx
+++ web/src/pages/studies/index.tsx
@@ -3,6 +3,7 @@ import Studies from "@/components/studies/Studies";
 import Button from "@/components/ui/Button";
 import Title from "@/components/ui/Title";
 import styles from "./StudiesPage.module.css";
+import { InfoIcon } from "@/components/assets/exports";
 
 export default function StudiesPage() {
   return (
@@ -13,8 +14,13 @@ export default function StudiesPage() {
       />
       <Title text={"Studies"} />
       <p className={styles.subtitle}>
-        Studies are a top level entity. They can contain projects and assets, for more detailed information and an
-        entity relationship diagram, look at our
+        Studies are a top level entity. They can contain projects{" "}
+        <InfoIcon title="A project is owned by a study and can contain assets" className={styles.info} /> and assets{" "}
+        <InfoIcon
+          title="Assets can be owned directly by a study or by a project within a study"
+          className={styles.info}
+        />
+        , for more detailed information and an entity relationship diagram, look at our
         <Button href="/glossary#studies" variant="tertiary" size="small" className={styles["glossary-button"]}>
           Glossary
         </Button>
