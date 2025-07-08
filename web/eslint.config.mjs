c590d185bb5ab8236e87300147be1373de02e593
acholyn <a.ho-lyn@ucl.ac.uk>
2025-07-08 11:28:33 +0100

    putting diagram on glossary page

--- web/eslint.config.mjs
+++ web/eslint.config.mjs
@@ -13,7 +13,12 @@ const eslintConfig = [
   {
     ignores: ["out/**", "node_modules/**", ".next/**", "dist/**", "cypress/**", "cypress.config.ts", "src/openapi/**"],
   },
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    extends: ["next/core-web-vitals", "next/typescript"],
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+    },
+  }),
 ];
 
 export default eslintConfig;
--- web/src/pages/glossary/index.tsx
+++ web/src/pages/glossary/index.tsx
@@ -1,5 +1,6 @@
 import MetaHead from "@/components/meta/Head";
 import Title from "@/components/ui/Title";
+import styles from "./GlossaryPage.module.css";
 
 export default function GlossaryPage() {
   return (
@@ -9,7 +10,16 @@ export default function GlossaryPage() {
         description="Definitions and diagrams for terminology used in the ARC Services Portal"
       />
       <Title text={"Glossary"} />
-      <p>This page is being built. Please check back soon for updates!</p>
+      <p>
+        This page is being built. Please check back soon for updates! Below is a basic diagram of how our entities are
+        related
+      </p>
+
+      <img
+        src={"/entity_relationships.png"}
+        alt="Entity diagram demonstrating that studies are top level entities, with projects and assets as children. Projects can also contain assets"
+        className={styles["entity-relationships"]}
+      />
     </>
   );
 }
