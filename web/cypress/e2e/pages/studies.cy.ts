5f37234e1380a7f916aa643f3004ce77096ffb37
Amanda Ho-Lyn <72266023+acholyn@users.noreply.github.com>
2025-07-14 16:01:58 +0100

    Amanda/5 studies (#152)
    * adding studies button on profile if complete
    
    * basic glossary page so link doesn't fail
    
    * basic approved researcher view on studies page
    
    * slight restructure + styling
    
    * updating roles if manual intervention
    
    * 🐪 -> 🍡 for css classes
    
    * AdminDialog -> Dialog
    
    * slight refactor of dialog to make more reusable
    
    * basic form
    
    * adding react hook form
    
    * making form link into global css var
    
    * adding react hook form + styling to form
    
    * adding in uikit components
    
    * rm updating roles in people service
    
    * setting study owner to user
    
    * rm data security section
    
    * tidying
    
    * improvements to steps + moving checkboxes before statements
    
    * style tweaks + new helper text for owner and form if not valid
    
    * dot progress
    
    * owner readonly instead of disabled + if first page valid, step stays green
    
    * initial tests; checking readonly
    
    * fixing readonly class
    
    * fixing cypress structure
    
    * cypressId -> cyid due to react error
    
    * more for tests
    
    * cyid -> cy
    
    * uikit checkbox -> html checkbox
    
    uikit input doesn't work well as checkbox with RHF + uikit has an upcoming checkbox component
    
    * tweaks
    
    * basic tooltips for project and asstes
    
    * styling tweaks to info tooltips
    
    * putting diagram on glossary page
    
    * fixing rule
    
    * removing study prefix + making it more obvious you need to be an AR to view studies
    
    * fixing valid on steps
    
    * fixing tests
    
    * placeholder + validation for admin field
    
    * Update web/src/components/studies/CreateStudyForm.tsx
    
    Co-authored-by: Finley Bacon <162155673+finleybacon@users.noreply.github.com>
    
    * name + description
    
    * interface -> type
    
    * removing admin check
    
    * info tooltip component
    
    * tweaks to info tooltip
    
    * using native select + inputs for data protection number
    
    * using month input instead of date
    
    * adding helper text for the dp number
    
    * fixing typo in test
    
    * drawio assets
    
    * test typo
    
    * updating controller check in test
    
    * adding inline attribute to button + fixing profile button on studies for base
    
    * wrapping controller input in RHF controller
    
    * allowing free input for registry id if not ucl as controller
    
    * installing logger and koanf
    
    * testing conditional fields
    
    * precommit
    
    * precommit
    
    * fixing tests
    
    ---------
    
    Co-authored-by: Finley Bacon <162155673+finleybacon@users.noreply.github.com>

---
+++ web/cypress/e2e/pages/studies.cy.ts
@@ -0,0 +1,135 @@
+beforeEach(() => {
+  cy.clearCookies();
+  cy.clearLocalStorage();
+});
+
+describe(`Studies page content`, () => {
+  it("should show base role content", () => {
+    cy.loginAsBase();
+    cy.mockAuthAsBaseUser();
+    cy.visit("/studies");
+    cy.waitForAuth();
+
+    cy.contains("No Studies").should("be.visible");
+  });
+
+  it("should show content for base approved researcher", () => {
+    cy.loginAsBase();
+    cy.mockAuthAsBaseApprovedResearcher();
+    cy.visit("/studies");
+    cy.waitForAuth();
+
+    cy.contains("No Studies").should("not.exist");
+    cy.contains("Your Studies").should("be.visible");
+  });
+});
+
+describe("Approved researcher can create studies", () => {
+  beforeEach(() => {
+    cy.loginAsBase();
+    cy.mockAuthAsBaseApprovedResearcher();
+    cy.visit("/studies");
+    cy.waitForAuth();
+  });
+
+  it("can bring up the form", () => {
+    cy.contains("Create Study").should("be.visible").click();
+    cy.get("[data-cy='create-study-form']").should("be.visible");
+  });
+
+  it("owner set to user & can't be edited", () => {
+    cy.fixture("auth-base-approved-researcher.json").then((data) => {
+      const username = data.username;
+
+      cy.contains("Create Study").should("be.visible").click();
+
+      cy.get("input[name='owner']").should("have.attr", "readonly");
+      cy.get("input[name='owner']").should("have.value", username);
+    });
+  });
+
+  it("can't submit without filling in required fields", () => {
+    cy.contains("Create Study").should("be.visible").click();
+
+    // click through form
+    cy.get("[data-cy='next']").click();
+    cy.get("[data-cy='next']").click();
+    cy.get("button[type='submit']").should("be.disabled");
+
+    // go back to start of form and fill in the fields
+    cy.get("[data-cy='back']").click();
+    cy.get("[data-cy='back']").click();
+    cy.get("input[name='studyName']").type("Test Study");
+    cy.get("select[name='controller']").select("UCL");
+
+    // go to the end and submit
+    cy.get("[data-cy='next']").click();
+    cy.get("[data-cy='next']").click();
+    cy.get("button[type='submit']").should("not.be.disabled");
+  });
+});
+
+describe("Checking conditionally rendered fields", () => {
+  beforeEach(() => {
+    cy.loginAsBase();
+    cy.mockAuthAsBaseApprovedResearcher();
+    cy.visit("/studies");
+    cy.waitForAuth();
+    cy.contains("Create Study").click();
+  });
+  afterEach(() => {
+    cy.get("[data-cy='close-dialog']").click();
+  });
+
+  it("shows controller text input if not UCL", () => {
+    cy.get("select[name='controller']").select("Other");
+    cy.get("input[name='controllerOther']").type("Test Controller");
+  });
+
+  it("sets DPO id to UCL's if UCL selected", () => {
+    cy.get("select[name='controller']").select("UCL");
+
+    cy.get("[data-cy='next']").click();
+    cy.get("[data-cy='next']").click();
+
+    cy.get("input[name='dataProtection']").check();
+    cy.get("input[name='dataProtectionPrefix']")
+      .should("have.value", "Z6364106")
+      .should("have.attr", "readonly", "readonly");
+  });
+
+  it("allows DPO id to be set if not UCL", () => {
+    cy.get("select[name='controller']").select("Other");
+
+    cy.get("[data-cy='next']").click();
+    cy.get("[data-cy='next']").click();
+
+    cy.get("input[name='dataProtection']").check();
+    cy.get("input[name='dataProtectionPrefix']").should("not.have.attr", "readonly");
+    cy.get("input[name='dataProtectionPrefix']").type("A12345678"); // pragma: allowlist secret
+  });
+
+  it("should show CAG ref when checked", () => {
+    cy.get("[data-cy='next']").click();
+    cy.get("input[name='cag']").check();
+
+    cy.get("input[name='cagRef']").should("be.visible");
+  });
+
+  it("should show IRAS ID when HRA is checked", () => {
+    cy.get("[data-cy='next']").click();
+    cy.get("input[name='hra']").check();
+
+    cy.get("input[name='irasId']").should("be.visible");
+  });
+
+  it("should show NHS questions when checked", () => {
+    cy.get("[data-cy='next']").click();
+    cy.get("input[name='nhs']").check();
+
+    cy.get("input[name='nhsEngland']").should("be.visible").check();
+    cy.get("input[name='nhsEnglandRef']").should("be.visible");
+    cy.get("input[name='mnca']").should("be.visible");
+    cy.get("input[name='dspt']").should("be.visible");
+  });
+});
