75d76d0620f12d6ec0ddb9409f3da50027dde1f2
acholyn <a.ho-lyn@ucl.ac.uk>
2025-07-14 13:07:38 +0100

    testing conditional fields

--- web/cypress/e2e/pages/studies.cy.ts
+++ web/cypress/e2e/pages/studies.cy.ts
@@ -60,12 +60,71 @@ describe("Approved researcher can create studies", () => {
     cy.get("[data-cy='back']").click();
     cy.get("[data-cy='back']").click();
     cy.get("input[name='studyName']").type("Test Study");
-    cy.get("select[name='controller']").select("Other");
-    cy.get("input[name='controllerOther']").type("Test Controller");
+    cy.get("select[name='controller']").select("UCL");
 
     // go to the end and submit
     cy.get("[data-cy='next']").click();
     cy.get("[data-cy='next']").click();
     cy.get("button[type='submit']").should("not.be.disabled");
+  });
+});
+
+describe("Checking conditionally rendered fields", () => {
+  beforeEach(() => {
+    cy.loginAsBase();
+    cy.mockAuthAsBaseApprovedResearcher();
+    cy.visit("/studies");
+    cy.waitForAuth();
+    cy.contains("Create Study").click();
+  });
+
+  it("shows controller text input if not UCL", () => {
+    cy.get("select[name='controller']").select("Other");
+    cy.get("input[name='controllerOther']").type("Test Controller");
+  });
+
+  it("sets DPO id to UCL's if UCL selected", () => {
+    cy.get("select[name='controller']").select("UCL");
+
+    cy.get("[data-cy='next']").click();
+    cy.get("[data-cy='next']").click();
+
+    cy.get("input[name='dataProtection']").check();
+    cy.get("input[name='dataProtectionPrefix']").should("have.value", "Z6364106").should("have.attr", "readonly");
+  });
+
+  it("allows DPO id to be set if not UCL", () => {
+    cy.get("select[name='controller']").select("Other");
+
+    cy.get("[data-cy='next']").click();
+    cy.get("[data-cy='next']").click();
+
+    cy.get("input[name='dataProtection']").check();
+
+    cy.get("input[name='dataProtectionPrefix']").should("not.have.attr", "readonly").type("A12345678"); // pragma: allowlist secret
+  });
+
+  it("should show CAG ref when checked", () => {
+    cy.get("[data-cy='next']").click();
+    cy.get("input[name='cag']").check();
+
+    cy.get("input[name='cagRef']").should("be.visible");
+  });
+
+  it("should show IRAS ID when HRA is checked", () => {
+    cy.get("[data-cy='next']").click();
+    cy.get("input[name='hra']").check();
+
+    cy.get("input[name='irasId']").should("be.visible");
+  });
+
+  it("should show NHS questions when checked", () => {
+    cy.get("[data-cy='next']").click();
+    cy.get("input[name='nhs']").check();
+
+    cy.get("input[name='nhsEngland']").should("be.visible").check();
+    cy.get("input[name='nhsEnglandRef']").should("be.visible");
+    cy.get("input[name='mnca']").should("be.visible");
+    cy.get("input[name='dspt']").should("be.visible");
   });
 });
