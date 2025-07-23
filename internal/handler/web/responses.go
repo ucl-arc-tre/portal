36620b28d257e4493a8a01c1d01925e7b940afbc
t-young31 <tom.young.316@gmail.com>
2025-07-23 16:20:04 +0100

    use error types

--- internal/handler/web/responses.go
+++ internal/handler/web/responses.go
@@ -15,22 +15,19 @@ func setError(ctx *gin.Context, err error, message string) {
 	if err == nil {
 		panic(fmt.Errorf("set error called with nil error [%v]", message))
 	}
+	statusCode := 520
 	if errors.Is(err, types.ErrServerError) {
-		setErrResponse(ctx, err, http.StatusInternalServerError, message)
-		return
+		statusCode = http.StatusInternalServerError
 	} else if errors.Is(err, types.ErrInvalidObject) {
-		setErrResponse(ctx, err, http.StatusNotAcceptable, message)
-		return
+		statusCode = http.StatusNotAcceptable
+	} else if errors.Is(err, types.ErrNotFound) {
+		statusCode = http.StatusNotFound
+	} else {
+		err = fmt.Errorf("unknown error: %v", err)
 	}
-	setErrResponse(ctx, fmt.Errorf("unknown error: %v", err), 520, message)
-}
-
-func setErrResponse(ctx *gin.Context, err error, status int, message string) {
 	user := middleware.GetUser(ctx)
-	if err != nil {
-		log.Err(err).Any("username", user.Username).Msg(message)
-		ctx.Status(status)
-	}
+	log.Err(err).Any("username", user.Username).Msg(message)
+	ctx.Status(statusCode)
 }
 
 func bindJSONOrSetError(ctx *gin.Context, obj any) error {
--- internal/types/errors.go
+++ internal/types/errors.go
@@ -8,6 +8,7 @@ import (
 var (
 	ErrInvalidObject = errors.New("invalid object")
 	ErrServerError   = errors.New("server error")
+	ErrNotFound      = errors.New("not found")
 )
 
 func NewErrInvalidObject(err error) error {
@@ -16,6 +17,10 @@ func NewErrInvalidObject(err error) error {
 
 func NewErrServerError(err error) error {
 	return newErrorWithType(err, ErrServerError)
+}
+
+func NewNotFoundError(err error) error {
+	return newErrorWithType(err, ErrNotFound)
 }
 
 func newErrorWithType(err error, errorType error) error {
