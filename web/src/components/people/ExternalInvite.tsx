58d347270c8a1998eb2353258b5172a1ebf3b279
acholyn <a.ho-lyn@ucl.ac.uk>
2025-07-31 16:40:29 +0100

    smol sad modal

--- web/src/components/people/ExternalInvite.tsx
+++ web/src/components/people/ExternalInvite.tsx
@@ -1,19 +1,22 @@
 import Button from "@/components/ui/Button";
 import { useState } from "react";
-import { Input } from "../shared/exports";
+import { Input, Alert, AlertMessage, Label } from "../shared/exports";
 import styles from "./ExternalInvite.module.css";
 import { postUsersInvite } from "@/openapi";
 import Loading from "@/components/ui/Loading";
+import Dialog from "../ui/Dialog";
 
 export default function ExternalInvite() {
   const [buttonDisabled, setButtonDisabled] = useState(false);
-  const [isInputVisible, setInputVisible] = useState(false);
+  const [isDialogVisible, setDialogVisible] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
   const [email, setEmail] = useState("");
   const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+  const [showErrorMessage, setShowErrorMessage] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
 
-  function handleShowInput() {
-    setInputVisible(true);
+  function handleShowDialog() {
+    setDialogVisible(true);
   }
 
   async function handleSumbit(e: React.FormEvent<HTMLFormElement>) {
@@ -26,6 +29,14 @@ export default function ExternalInvite() {
       }
     } catch (err) {
       console.error("Invite post error:", err);
+      let errMessage;
+      if (err instanceof Error) {
+        errMessage = err.message;
+      } else {
+        errMessage = "An unknown error occurred";
+      }
+      setErrorMessage(errMessage);
+      setShowErrorMessage(true);
     } finally {
       setButtonDisabled(false);
       setIsLoading(false);
@@ -35,40 +46,55 @@ export default function ExternalInvite() {
   }
 
   return (
-    <div className={styles.container}>
-      {showSuccessMessage && (
-        <small onClick={() => setShowSuccessMessage(false)} className={styles["success-message"]}>
-          Invitation sent!
-        </small>
+    <>
+      {isDialogVisible && (
+        <Dialog setDialogOpen={setDialogVisible}>
+          <div>
+            {showSuccessMessage && (
+              <small onClick={() => setShowSuccessMessage(false)} className={styles["success-message"]}>
+                &times; Invitation sent!
+              </small>
+            )}
+            {showErrorMessage && (
+              <Alert type="error">
+                <AlertMessage>{errorMessage}</AlertMessage>
+              </Alert>
+            )}
+            <form onSubmit={handleSumbit} className={styles["invite-form"]}>
+              <Label htmlFor="email">Invite a researcher to the portal</Label>
+              <Input
+                type="email"
+                id="email"
+                placeholder="Email address"
+                name="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <Button disabled={buttonDisabled} type="submit" cy="send-invite" className={styles["send-button"]}>
+                {isLoading && (
+                  <span className={styles.loader}>
+                    <Loading message="" size="small" />
+                  </span>
+                )}
+                Send Invitation
+              </Button>{" "}
+            </form>
+          </div>
+        </Dialog>
       )}
-      <form onSubmit={handleSumbit} className={`${styles["slide-fade-down"]} ${isInputVisible ? "" : styles.hidden}`}>
-        <Input
-          type="email"
-          placeholder="Email address"
-          name="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <Button disabled={buttonDisabled} type="submit" cy="send-invite" size="small" className={styles["send-button"]}>
-          {isLoading && (
-            <span className={styles.loader}>
-              <Loading message="" size="small" />
-            </span>
-          )}
-          Send Invitation
-        </Button>{" "}
-      </form>
-      <Button
-        className={`${styles["slide-right-fade"]} ${isInputVisible ? styles.hidden : ""}`}
-        onClick={handleShowInput}
-        disabled={buttonDisabled}
-        variant="secondary"
-        cy="show-invite-input"
-        type="button"
-        size="small"
-      >
-        Invite external researcher
-      </Button>
-    </div>
+
+      <div className={styles.container}>
+        <Button
+          onClick={handleShowDialog}
+          disabled={buttonDisabled}
+          variant="secondary"
+          cy="show-invite-input"
+          type="button"
+          size="small"
+        >
+          Invite external researcher
+        </Button>
+      </div>
+    </>
   );
 }
