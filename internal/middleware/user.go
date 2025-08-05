9bc7ee6a66c1d619a38b2e429533c59982e6f809
Tom Young <39765193+t-young31@users.noreply.github.com>
2025-08-05 10:25:55 +0100

    fix base role assignment (#215)
    * fix base role assignment
    
    * return if user was created

--- internal/middleware/user.go
+++ internal/middleware/user.go
@@ -1,17 +1,15 @@
 package middleware
 
 import (
-	"fmt"
 	"net/http"
 	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/hashicorp/golang-lru/v2/expirable"
 	"github.com/rs/zerolog/log"
-	"github.com/ucl-arc-tre/portal/internal/graceful"
 	"github.com/ucl-arc-tre/portal/internal/rbac"
+	"github.com/ucl-arc-tre/portal/internal/service/users"
 	"github.com/ucl-arc-tre/portal/internal/types"
-	"gorm.io/gorm"
 )
 
 const (
@@ -26,13 +24,13 @@ func GetUser(ctx *gin.Context) types.User {
 }
 
 type UserSetter struct {
-	db    *gorm.DB
+	users *users.Service
 	cache *expirable.LRU[types.Username, types.User]
 }
 
 func NewSetUser() gin.HandlerFunc {
 	setter := &UserSetter{
-		db:    graceful.NewDB(),
+		users: users.New(),
 		cache: expirable.NewLRU[types.Username, types.User](1000, nil, userCacheTTL),
 	}
 	return setter.setUser
@@ -47,34 +45,14 @@ func (u *UserSetter) setUser(ctx *gin.Context) {
 		ctx.Set(userContextKey, user)
 		return
 	}
-	user, err := u.persistedUser(username)
+	user, _, err := u.users.PersistedUser(username)
 	if err != nil {
 		log.Err(err).Msg("Failed to get user")
 		ctx.AbortWithStatus(http.StatusInternalServerError)
 		return
 	}
+	if hasBaseRole, err := rbac.HasRole(user, rbac.Base); err != nil && !hasBaseRole {
+		_, _ = rbac.AddRole(user, rbac.Base)
+	}
 	ctx.Set(userContextKey, user)
-}
-
-// Get a user that's persisted in the database by their username. If
-// they do not exist then they will be created with the base role
-func (u *UserSetter) persistedUser(username types.Username) (types.User, error) {
-	user := types.User{}
-	result := u.db.Where("username = ?", username).
-		Attrs(types.User{
-			Username: username,
-			Model:    types.Model{CreatedAt: time.Now()},
-		}).
-		FirstOrCreate(&user)
-	if result.Error != nil {
-		return user, types.NewErrServerError(result.Error)
-	}
-	userWasCreated := result.RowsAffected > 0
-	if userWasCreated {
-		_, err := rbac.AddRole(user, rbac.Base)
-		if err != nil {
-			return user, fmt.Errorf("failed assign user base role: %v", err)
-		}
-	}
-	return user, nil
 }
