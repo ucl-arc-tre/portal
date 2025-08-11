0833d395d85cd580b1a4ddcfe32c23d9c52860a1
acholyn <a.ho-lyn@ucl.ac.uk>
2025-08-11 12:27:54 +0100

    index page fixes: title alignment (using component), user tasks width + alignment

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,6 +1,7 @@
 import styles from "./index.module.css";
 import MetaHead from "@/components/meta/Head";
 import UserTasks from "@/components/index/UserTasks";
+import Title from "@/components/ui/Title";
 
 export default function Index() {
   return (
@@ -8,11 +9,13 @@ export default function Index() {
       <MetaHead title="ARC Services Portal | UCL" description="ARC Services Portal homepage" />
 
       <div className={styles.title}>
-        <h1>Welcome to the ARC Services Portal</h1>
+        <Title text={"Welcome to the ARC Services Portal"} />
         <p>This portal allows UCL researchers to manage ARC services and tasks.</p>
       </div>
 
-      <UserTasks />
+      <div className={styles["task-wrapper"]}>
+        <UserTasks />
+      </div>
     </>
   );
 }
