481db09b7513965ff4a96f0e2da1e133f2464715
t-young31 <tom.young.316@gmail.com>
2025-08-12 11:04:30 +0100

    simplify agreements

--- internal/handler/web/agreements.go
+++ internal/handler/web/agreements.go
@@ -1,35 +1,34 @@
 package web
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 	openapi "github.com/ucl-arc-tre/portal/internal/openapi/web"
+	"github.com/ucl-arc-tre/portal/internal/types"
 )
 
 func (h *Handler) GetAgreementsAgreementType(ctx *gin.Context, agreementType openapi.AgreementType) {
+	var agreement *types.Agreement
+	var err error
+
 	switch agreementType {
 	case openapi.AgreementTypeApprovedResearcher:
-		agreement, err := h.agreements.LatestApprovedResearcher()
-		if err != nil {
-			setError(ctx, err, "Failed to get approved researcher agreement")
-			return
-		}
-		ctx.JSON(http.StatusOK, openapi.Agreement{
-			Id:   agreement.ID.String(),
-			Text: agreement.Text,
-		})
+		agreement, err = h.agreements.LatestApprovedResearcher()
 	case openapi.AgreementTypeStudyOwner:
-		agreement, err := h.agreements.LatestStudyOwner()
-		if err != nil {
-			setError(ctx, err, "Failed to get study owner agreement")
-			return
-		}
-		ctx.JSON(http.StatusOK, openapi.Agreement{
-			Id:   agreement.ID.String(),
-			Text: agreement.Text,
-		})
+		agreement, err = h.agreements.LatestStudyOwner()
 	default:
-		ctx.JSON(http.StatusOK, http.StatusNotFound)
+		ctx.Status(http.StatusNotFound)
+		return
 	}
+
+	if err != nil {
+		setError(ctx, err, fmt.Sprintf("Failed to get agreement of type [%v]", agreementType))
+		return
+	}
+	ctx.JSON(http.StatusOK, openapi.Agreement{
+		Id:   agreement.ID.String(),
+		Text: agreement.Text,
+	})
 }
