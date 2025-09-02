4b3aac4e858cccc4607c2bf420f06ac671020124
acholyn <a.ho-lyn@ucl.ac.uk>
2025-09-02 12:39:15 +0100

    splitting out GetUsers

--- internal/handler/web/users.go
+++ internal/handler/web/users.go
@@ -33,55 +33,62 @@ func (h *Handler) GetUsers(ctx *gin.Context, params openapi.GetUsersParams) {
 
 	query := params.Find
 
+	// retrieve auth + agreements + training info for set of users
 	if isAdmin {
-		// retrieve auth + agreements + training info
-		if query != nil {
-			people, err := h.users.SearchEntraForUsersAndMatch(ctx, *query)
-			if err != nil {
-				setError(ctx, err, "Failed to find people in tenant")
-				return
-			}
-			ctx.JSON(http.StatusOK, people)
-
-		} else {
-			people, err := h.users.AllUsers()
-			if err != nil {
-				setError(ctx, err, "Failed to get people")
-				return
-			}
-			ctx.JSON(http.StatusOK, people)
-		}
+		h.getUsersAdmin(ctx, query)
 
 	} else if isTreOpsStaff {
-		if query != nil {
-			users, err := h.users.SearchEntraForUsersAndMatch(ctx, *query)
-			if err != nil {
-				setError(ctx, err, "Failed to find people in tenant")
-				return
-			}
-
-			people := []openapi.UserData{}
-
-			for _, userData := range users {
-				// only carry over users with approved researcher role
-				if slices.Contains(userData.Roles, "approved_researcher") {
-					people = append(people, userData)
-				}
-			}
-			ctx.JSON(http.StatusOK, people)
-
-		} else {
-			// retrieve auth + agreements + training info
-			people, err := h.users.AllApprovedResearcherUsers()
-			if err != nil {
-				setError(ctx, err, "Failed to get people")
-				return
-			}
-			ctx.JSON(http.StatusOK, people)
-		}
+		h.getUsersTreOps(ctx, query)
 
 	} else {
 		ctx.JSON(http.StatusInternalServerError, "Not implemented")
+	}
+}
+
+func (h *Handler) getUsersAdmin(ctx *gin.Context, query *string) {
+	if query != nil {
+		people, err := h.users.SearchEntraForUsersAndMatch(ctx, *query)
+		if err != nil {
+			setError(ctx, err, "Failed to find people in tenant")
+			return
+		}
+		ctx.JSON(http.StatusOK, people)
+
+	} else {
+		people, err := h.users.AllUsers()
+		if err != nil {
+			setError(ctx, err, "Failed to get people")
+			return
+		}
+		ctx.JSON(http.StatusOK, people)
+	}
+}
+
+func (h *Handler) getUsersTreOps(ctx *gin.Context, query *string) {
+	if query != nil {
+		users, err := h.users.SearchEntraForUsersAndMatch(ctx, *query)
+		if err != nil {
+			setError(ctx, err, "Failed to find people in tenant")
+			return
+		}
+
+		people := []openapi.UserData{}
+
+		for _, userData := range users {
+			// only carry over users with approved researcher role
+			if slices.Contains(userData.Roles, "approved_researcher") {
+				people = append(people, userData)
+			}
+		}
+		ctx.JSON(http.StatusOK, people)
+
+	} else {
+		people, err := h.users.AllApprovedResearcherUsers()
+		if err != nil {
+			setError(ctx, err, "Failed to get people")
+			return
+		}
+		ctx.JSON(http.StatusOK, people)
 	}
 }
 
