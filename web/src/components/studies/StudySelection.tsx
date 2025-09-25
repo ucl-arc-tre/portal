2c3426030567bd82e03a154c284159d310563bf9
acholyn <a.ho-lyn@ucl.ac.uk>
2025-09-25 15:35:04 +0100

    sorting studies on frontend

--- web/src/components/studies/StudySelection.tsx
+++ web/src/components/studies/StudySelection.tsx
@@ -14,29 +14,38 @@ export default function StudySelection(props: StudySelectionProps) {
   const { studies, isAdmin } = props;
   const router = useRouter();
 
+  const studySortOrder = {
+    Pending: 1,
+    Incomplete: 2,
+    Rejected: 3,
+    Approved: 4,
+  };
+
   return (
     <div className={styles["study-selection"]}>
       {!isAdmin && <h2 className={styles["studies-heading"]}>Your Studies</h2>}
 
       <div className={styles["studies-list"]}>
-        {studies.map((study) => (
-          <div key={study.id} className={styles["study-card"]}>
-            <div className={styles["status-indicator"]}>
-              <StudyStatusBadge status={study.approval_status} isAdmin={isAdmin} />
-            </div>
+        {studies
+          .sort((a, b) => studySortOrder[a.approval_status] - studySortOrder[b.approval_status])
+          .map((study) => (
+            <div key={study.id} className={styles["study-card"]}>
+              <div className={styles["status-indicator"]}>
+                <StudyStatusBadge status={study.approval_status} isAdmin={isAdmin} />
+              </div>
 
-            <div className={styles["study-info"]}>
-              <h3 className={styles["study-title"]}>{study.title}</h3>
-              <p className={styles["study-description"]}>{study.description}</p>
-            </div>
+              <div className={styles["study-info"]}>
+                <h3 className={styles["study-title"]}>{study.title}</h3>
+                <p className={styles["study-description"]}>{study.description}</p>
+              </div>
 
-            <div className={styles["study-actions"]}>
-              <Button onClick={() => router.push(`/studies/manage?studyId=${study.id}`)} size="small">
-                {isAdmin ? "View Study" : "Manage Study"}
-              </Button>
+              <div className={styles["study-actions"]}>
+                <Button onClick={() => router.push(`/studies/manage?studyId=${study.id}`)} size="small">
+                  {isAdmin ? "View Study" : "Manage Study"}
+                </Button>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
       </div>
     </div>
   );
