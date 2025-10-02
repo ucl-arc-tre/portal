0848a92d26af566d5bb03e9b89031be9802e53fb
acholyn <a.ho-lyn@ucl.ac.uk>
2025-10-02 16:52:00 +0100

    rm iao rbac

--- internal/rbac/init.go
+++ internal/rbac/init.go
@@ -49,7 +49,6 @@ func addApprovedResearcherPolicies(enforcer *casbin.Enforcer) {
 		{RoleName: ApprovedStaffResearcher, Resource: "/studies", Action: WriteAction},
 		{RoleName: ApprovedStaffResearcher, Resource: "/agreements/study-owner", Action: ReadAction},
 		{RoleName: InformationAssetOwner, Resource: "/users/invite", Action: WriteAction},
-		{RoleName: InformationAssetOwner, Resource: "/studies/<studyId>/review", Action: WriteAction},
 	}
 	for _, policy := range policies {
 		mustAddPolicy(enforcer, policy)
