593a5ad29c2ab12e237353d9b69f8efa415cf871
acholyn <a.ho-lyn@ucl.ac.uk>
2025-10-07 15:22:05 +0100

    abstracting mail func to be more reusable

--- internal/controller/entra/custom_mail.go
+++ internal/controller/entra/custom_mail.go
@@ -6,40 +6,37 @@ import (
 	graphmodels "github.com/microsoftgraph/msgraph-sdk-go/models"
 	graphusers "github.com/microsoftgraph/msgraph-sdk-go/users"
 	"github.com/ucl-arc-tre/portal/internal/config"
+	openapi "github.com/ucl-arc-tre/portal/internal/openapi/web"
 	"github.com/ucl-arc-tre/portal/internal/types"
 )
 
-func (c *Controller) SendCustomInviteNotification(ctx context.Context, email string, sponsor types.Sponsor) error {
-	// use graph to send email saying so-and-so has invited you to the portal
+func (c *Controller) createCustomEmail(ctx context.Context, subject string, emails []string, content string) error {
+	// set up the email
 
 	requestBody := graphusers.NewItemSendMailPostRequestBody()
 	message := graphmodels.NewMessage()
-	subject := "Notification: You have been invited to the UCL ARC Services Portal"
-	message.SetSubject(&subject)
+	message.SetSubject(&subject) // set from args
 	body := graphmodels.NewItemBody()
 	contentType := graphmodels.HTML_BODYTYPE
 	body.SetContentType(&contentType)
 
-	content := ""
-	if sponsor.ChosenName != "" {
-		content = "You have been invited to join the UCL ARC Services Portal by " + string(sponsor.ChosenName)
-	} else {
-		content = "You have been invited to join the UCL ARC Services Portal by " + string(sponsor.Username)
-	}
-	content = content + "<br><br>You can sign into the portal at <a href='https://portal.arc.ucl.ac.uk'>https://portal.arc.ucl.ac.uk</a> <br><br> This is an automated message, but this mailbox is monitored so you can contact us if you need assistance."
-
-	body.SetContent(&content)
+	body.SetContent(&content) // set the content from the args
 	message.SetBody(body)
 
-	recipient := graphmodels.NewRecipient()
-	emailAddress := graphmodels.NewEmailAddress()
-	emailAddress.SetAddress(&email)
-	recipient.SetEmailAddress(emailAddress)
-
-	toRecipients := []graphmodels.Recipientable{
-		recipient,
+	recipients := []graphmodels.Recipientable{}
+	for _, email := range emails {
+		recipient := graphmodels.NewRecipient()
+		emailAddress := graphmodels.NewEmailAddress()
+		emailAddress.SetAddress(&email)
+		recipient.SetEmailAddress(emailAddress)
+		recipients = append(recipients, recipient)
 	}
-	message.SetToRecipients(toRecipients)
+	message.SetToRecipients(recipients)
+
+	// toRecipients := []graphmodels.Recipientable{
+	// 	recipient,
+	// }
+	// message.SetToRecipients(toRecipients)
 
 	requestBody.SetMessage(message)
 	saveToSentItems := true
@@ -54,4 +51,34 @@ func (c *Controller) SendCustomInviteNotification(ctx context.Context, email str
 	}
 
 	return nil
+
+}
+
+func (c *Controller) SendCustomInviteNotification(ctx context.Context, email string, sponsor types.Sponsor) error {
+	// use graph to send email saying so-and-so has invited you to the portal
+
+	content := ""
+	if sponsor.ChosenName != "" {
+		content = "You have been invited to join the UCL ARC Services Portal by " + string(sponsor.ChosenName)
+	} else {
+		content = "You have been invited to join the UCL ARC Services Portal by " + string(sponsor.Username)
+	}
+
+	return c.createCustomEmail(ctx, "Notification: You have been invited to the UCL ARC Services Portal", []string{email}, content)
+}
+
+func (c *Controller) SendCustomStudyReviewNotification(ctx context.Context, email string, review openapi.StudyReview) error {
+	// email to notify IAO + IAA when study has been reviewed
+
+	content := ""
+	subject := ""
+	if review.Status == openapi.Approved {
+		subject = "Notification: Your study has been approved!"
+		content = "Your study has been approved! You will be notified when you have any contracts or assets approaching expiry."
+	} else {
+		content = "Your study has not been approved, please review the feedback and request another review once the changes have been addressed. <br><br>Feedback: " + *review.Feedback
+		subject = "Notification: Your study has feedback to address"
+	}
+
+	return c.createCustomEmail(ctx, subject, []string{email}, content)
 }
