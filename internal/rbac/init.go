365e41ca130384d376181246cd70b40cbde2fb1e
James Hughes <70143648+jhughes982@users.noreply.github.com>
2025-11-07 10:47:31 +0000

    Update internal/rbac/init.go
    Co-authored-by: Tom Young <39765193+t-young31@users.noreply.github.com>

--- internal/rbac/init.go
+++ internal/rbac/init.go
@@ -49,8 +49,8 @@ func addApprovedResearcherPolicies(enforcer *casbin.Enforcer) {
 		{RoleName: ApprovedStaffResearcher, Resource: "/studies", Action: WriteAction},
 		{RoleName: ApprovedStaffResearcher, Resource: "/agreements/study-owner", Action: ReadAction},
 		{RoleName: InformationAssetOwner, Resource: "/users/invite", Action: WriteAction},
-		{RoleName: ApprovedResearcher, Resource: "/projects/*", Action: ReadAction},
-		{RoleName: ApprovedStaffResearcher, Resource: "/projects/*", Action: WriteAction},
+		{RoleName: ApprovedStaffResearcher, Resource: "/projects/tre", Action: ReadAction},
+		{RoleName: ApprovedStaffResearcher, Resource: "/projects/tre", Action: WriteAction},
 	}
 	for _, policy := range policies {
 		mustAddPolicy(enforcer, policy)
