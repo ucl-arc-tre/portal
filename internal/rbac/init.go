7e2464b9e80d00f744823f3482af33ca43445fe7
James Hughes <james.hughes@ucl.ac.uk>
2025-11-07 10:25:16 +0000

    adding RBAC for projects

--- internal/rbac/init.go
+++ internal/rbac/init.go
@@ -49,6 +49,8 @@ func addApprovedResearcherPolicies(enforcer *casbin.Enforcer) {
 		{RoleName: ApprovedStaffResearcher, Resource: "/studies", Action: WriteAction},
 		{RoleName: ApprovedStaffResearcher, Resource: "/agreements/study-owner", Action: ReadAction},
 		{RoleName: InformationAssetOwner, Resource: "/users/invite", Action: WriteAction},
+		{RoleName: ApprovedResearcher, Resource: "/projects/*", Action: ReadAction},
+		{RoleName: ApprovedStaffResearcher, Resource: "/projects/*", Action: WriteAction},
 	}
 	for _, policy := range policies {
 		mustAddPolicy(enforcer, policy)
