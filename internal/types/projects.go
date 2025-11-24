ec83f8649018bfec30872ba7ab0bb1228b523b8a
James Hughes <70143648+jhughes982@users.noreply.github.com>
2025-11-24 14:18:10 +0000

    added additional researchers and draft status to projects (#389)
    * added additional researchers and draft status to projects
    
    * added project multi steps to form
    
    * WIP: adding draft types for project members and roles
    
    * WIP: tweaking model
    
    * WIP: idea for project roles setup
    
    * WIP: refining project role types
    
    * WIP: making role bindings environment specific
    
    * WIP: removed project members table
    
    * WIP: scaffolding project roles at the API level
    
    * small tweak
    
    * disabling projects
    
    * small tweak

---
+++ internal/types/projects.go
@@ -0,0 +1,50 @@
+package types
+
+import (
+	"github.com/google/uuid"
+)
+
+type Project struct {
+	ModelAuditable
+	Name          string    `gorm:"not null"`
+	CreatorUserID uuid.UUID `gorm:"not null;index"`
+	StudyID       uuid.UUID `gorm:"index"`
+	IsDraft       bool      `gorm:"not null;default:false"`
+
+	// Relationships
+	Study       Study `gorm:"foreignKey:StudyID"`
+	CreatorUser User  `gorm:"foreignKey:CreatorUserID"`
+}
+
+type ProjectTRE struct {
+	ModelAuditable
+	ProjectID                     uuid.UUID `gorm:"not null;index"`
+	EnvironmentID                 uuid.UUID `gorm:"not null;index"`
+	EgressNumberRequiredApprovals int       `gorm:"not null;default:1"`
+
+	// Relationships
+	Environment     Environment             `gorm:"foreignKey:EnvironmentID"`
+	Project         Project                 `gorm:"foreignKey:ProjectID"`
+	TRERoleBindings []ProjectTRERoleBinding `gorm:"foreignKey:ProjectTREID"`
+}
+
+type ProjectTRERoleName string
+
+const (
+	ProjectTREDesktopUser     ProjectTRERoleName = "desktop_user"     // allows access to a desktop
+	ProjectTREIngresser       ProjectTRERoleName = "ingresser"        // can upload data into the TRE
+	ProjectTREEgresser        ProjectTRERoleName = "egresser"         // can download data from the TRE
+	ProjectTREEgressRequester ProjectTRERoleName = "egress_requester" // can request data to be egressed
+	ProjectTREEgressChecker   ProjectTRERoleName = "egress_checker"   // can approve egress requests
+)
+
+type ProjectTRERoleBinding struct {
+	ModelAuditable
+	ProjectTREID uuid.UUID          `gorm:"not null;index"`
+	UserID       uuid.UUID          `gorm:"not null;index"`
+	Role         ProjectTRERoleName `gorm:"not null;index"`
+
+	// Relationships
+	ProjectTRE ProjectTRE `gorm:"foreignKey:ProjectTREID"`
+	User       User       `gorm:"foreignKey:UserID"`
+}
