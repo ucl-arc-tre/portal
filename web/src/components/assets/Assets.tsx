17b0db92a355821a82f907d85c457d172d5749d8
James Hughes <james.hughes@ucl.ac.uk>
2026-01-23 11:58:00 +0000

    fixing error

--- web/src/components/assets/Assets.tsx
+++ web/src/components/assets/Assets.tsx
@@ -2,7 +2,6 @@ import { useCallback, useEffect, useState } from "react";
 import {
   Asset,
   AssetBase,
-  ValidationError,
   getStudiesByStudyIdAssets,
   getStudiesByStudyIdAssetsByAssetIdContracts,
   postStudiesByStudyIdAssets,
