d5cf32e839924e75e9f5fcdff00db31899dfee46
acholyn <a.ho-lyn@ucl.ac.uk>
2026-02-12 16:45:27 +0000

    gosec

--- internal/config/types.go
+++ internal/config/types.go
@@ -3,7 +3,7 @@ package config
 type EntraCredentialBundle struct {
 	TenantID     string
 	ClientID     string
-	ClientSecret string
+	ClientSecret string // #nosec G117
 }
 
 type S3CredentialBundle struct {
