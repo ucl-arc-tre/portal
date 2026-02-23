13c05747dea3cfc7eb190f86a657f4d93c6a2f3c
Tom Young <39765193+t-young31@users.noreply.github.com>
2026-02-23 14:50:28 +0000

    add dsh api rate limiting (#511)

--- internal/middleware/ratelmit.go
+++ internal/middleware/ratelmit.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/go-chi/httprate"
+	"github.com/rs/zerolog/log"
 	"github.com/ucl-arc-tre/portal/internal/config"
 )
 
@@ -22,6 +23,29 @@ func NewWebRateLimit() gin.HandlerFunc {
 
 		key := ctx.GetHeader(usernameHeaderKey)
 		if onLimit(ctx.Writer, ctx.Request, key) {
+			ctx.AbortWithStatus(http.StatusTooManyRequests)
+			return
+		}
+		ctx.Next()
+	}
+
+	return handlerFunc
+}
+
+func NewDSHRateLimit() func(ctx *gin.Context) {
+	limiter := httprate.NewRateLimiter(100, time.Minute)
+
+	handlerFunc := func(ctx *gin.Context) {
+		// This keys by the real IP. The application should always be
+		// deployed behind a reverse proxy that sets the headers and
+		// are, as so, not trivially spoofable
+		key, err := httprate.KeyByRealIP(ctx.Request)
+		if err != nil {
+			log.Err(err).Msg("Failed to get real IP")
+			ctx.AbortWithStatus(http.StatusInternalServerError)
+			return
+		}
+		if limiter.OnLimit(ctx.Writer, ctx.Request, key) {
 			ctx.AbortWithStatus(http.StatusTooManyRequests)
 			return
 		}
