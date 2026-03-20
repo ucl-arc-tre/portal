fa055618689693566b84bfadb765900299e95c2c
acholyn <a.ho-lyn@ucl.ac.uk>
2026-03-20 16:25:05 +0000

    initial + styling

--- web/src/components/studies/IGOpsStudies.tsx
+++ web/src/components/studies/IGOpsStudies.tsx
@@ -5,7 +5,13 @@ import Button from "@/components/ui/Button";
 import { extractErrorMessage } from "@/lib/errorHandler";
 import styles from "./IGOpsStudies.module.css";
 import Loading from "../ui/Loading";
-import { Alert, AlertMessage } from "../shared/uikitExports";
+import { Alert, AlertMessage, HelperText } from "../shared/uikitExports";
+
+import dynamic from "next/dynamic";
+
+export const Search = dynamic(() => import("uikit-react-public").then((mod) => mod.Search), {
+  ssr: false,
+});
 
 export default function IGOpsStudies() {
   const [isLoading, setIsLoading] = useState(true);
@@ -32,8 +38,51 @@ export default function IGOpsStudies() {
     }
   };
 
+  const handleSearch = async (query: string) => {
+    console.log("Search query:", query);
+    setIsLoading(true);
+    setError(null);
+    try {
+      let response;
+      switch (true) {
+        case query.includes("caseref"):
+          response = await getStudies({ query: { query } });
+          break;
+        case query.includes("title"):
+          response = await getStudies({ query: { fuzzy_title: query.split("title:")[1] } });
+          break;
+        case query.includes("iao"):
+          response = await getStudies({ query: { owner_username: query.split("iao:")[1] } });
+          break;
+        case query.includes("status"):
+          response = await getStudies({ query: { query } });
+          break;
+        default:
+          response = await getStudies({ query: { query: query } });
+      }
+
+      if (!response.response.ok || !response.data) {
+        setError(`Search failed: ${extractErrorMessage(response)}`);
+        return;
+      }
+      console.log(response.data);
+      setStudies(response.data);
+    } catch (error) {
+      console.error("Search failed:", error);
+      setError("Search failed. Please try again.");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   useEffect(() => {
     fetchStudies();
+    document.getElementById("search")?.addEventListener("keydown", (e) => {
+      if (e.key === "Enter") {
+        const input = (e.target as HTMLInputElement).value;
+        handleSearch(input);
+      }
+    });
   }, [tab]);
 
   const emptyMessage = tab === "pending" ? "No studies pending approval" : "No studies found";
@@ -76,7 +125,15 @@ export default function IGOpsStudies() {
       )}
 
       {isLoading && <Loading message="Loading studies..." />}
-
+      <div>
+        <Search placeholder="Search Studies" onSearch={(q) => handleSearch(q)} id={styles.search} />
+        <HelperText>
+          <small>
+            You can use keywords to narrow your search: caseref, title, iao or status. eg. `caseref:12345`
+            <br></br>Note that `iao` will search IAO usernames (emails)
+          </small>
+        </HelperText>
+      </div>
       {!isLoading && studies.length === 0 && (
         <div className={styles["no-studies-message"]}>
           <h2>{emptyMessage}</h2>
