01cfd923a2f95a098bc76592a43be4e2b553adca
ucl_arc <ucl_arc@eduroam-int-dhcp-97-0-135.ucl.ac.uk>
2026-03-25 10:24:42 +0000

    Implement Go test utilities and add unit and integration tests for assets in studies service

---
+++ internal/service/studies/test_main_test.go
@@ -0,0 +1,68 @@
+package studies
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/ucl-arc-tre/portal/internal/testutil"
+	"github.com/ucl-arc-tre/portal/internal/types"
+	"gorm.io/gorm"
+)
+
+var testDBDSN string
+
+/*
+Set up for integration testing:
+- spin up a PostgreSQL container per package.
+- for each test: create a unique schema, run migrations within that schema,
+clean up (drop schema) after the test completes.
+
+Using TestMain to start a Postgres test container once, and
+this container will be used by all tests within this package
+*/
+func TestMain(m *testing.M) {
+	ctx := context.Background()
+
+	// start postgres container once
+	container, err := testutil.StartPostgresContainer(ctx)
+	if err != nil {
+		panic(err)
+	}
+
+	testDBDSN = container.DSN
+	fmt.Println("TestMain DSN:", testDBDSN)
+
+	// run tests
+	code := m.Run()
+
+	// terminate container
+	os.Exit(code)
+}
+
+// To be called by each test within this package to AutoMigrate
+// only the models/tables required by this package
+func migrate(db *gorm.DB) error {
+
+	// enable uuid extension
+	if err := db.Exec(`CREATE EXTENSION IF NOT EXISTS "uuid-ossp";`).Error; err != nil {
+		return err
+	}
+
+	// create sequence
+	if err := db.Exec(`CREATE SEQUENCE IF NOT EXISTS study_caseref_seq`).Error; err != nil {
+		return err
+	}
+
+	// Run migrations, only the models/tables required by this package
+	err := db.AutoMigrate(
+		&types.AssetLocation{},
+	)
+	if err != nil {
+		return err
+	}
+
+	return nil
+
+}
---
+++ internal/testutil/testutil.go
@@ -0,0 +1,118 @@
+package testutil
+
+import (
+	"context"
+	"fmt"
+
+	"time"
+
+	"github.com/stretchr/testify/require"
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/wait"
+
+	"testing"
+
+	"github.com/google/uuid"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type Container struct {
+	DSN       string
+	terminate func(context.Context) error
+}
+
+/*
+Setup Helper for integration test
+StartPostgresContainer starts a PostgreSQL container.
+Call this func from TestMain within each package.
+*/
+func StartPostgresContainer(ctx context.Context) (*Container, error) {
+	req := testcontainers.ContainerRequest{
+		Image:        "postgres:15",
+		ExposedPorts: []string{"5432/tcp"},
+		Env: map[string]string{
+			"POSTGRES_USER":     "postgres",
+			"POSTGRES_PASSWORD": "postgres",
+			"POSTGRES_DB":       "testdb",
+		},
+		WaitingFor: wait.ForListeningPort("5432/tcp").
+			WithStartupTimeout(30 * time.Second),
+	}
+
+	container, err := testcontainers.GenericContainer(ctx,
+		testcontainers.GenericContainerRequest{
+			ContainerRequest: req,
+			Started:          true,
+		},
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	host, err := container.Host(ctx)
+	if err != nil {
+		return nil, err
+	}
+	port, err := container.MappedPort(ctx, "5432")
+	if err != nil {
+		return nil, err
+	}
+
+	dsn := fmt.Sprintf(
+		"host=%s port=%s user=postgres password=postgres dbname=testdb sslmode=disable",
+		host, port.Port(),
+	)
+
+	return &Container{
+		DSN: dsn,
+		terminate: func(ctx context.Context) error {
+			return container.Terminate(ctx)
+		},
+	}, nil
+}
+
+type Migrator func(*gorm.DB) error
+
+/*
+Setup Helper for integration test
+NewTestDBSchema creates a new database schema.
+Call this func from each test within all packages.
+*/
+func NewTestDBSchema(t *testing.T, baseDSN string, migrate Migrator) *gorm.DB {
+	t.Helper()
+
+	require := require.New(t)
+
+	schema := "test_" + uuid.NewString()
+
+	// Admin connection
+	adminDB, err := gorm.Open(postgres.Open(baseDSN), &gorm.Config{})
+	require.NoError(err, "admin connect")
+
+	// Create schema
+	err = adminDB.Exec(fmt.Sprintf(`CREATE SCHEMA "%s"`, schema)).Error
+	require.NoError(err, "create schema")
+
+	// Connect with search_path
+	dsn := fmt.Sprintf("%s search_path=%s", baseDSN, schema)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	require.NoError(err, "connect test db")
+
+	// Run migrations
+	err = migrate(db)
+	require.NoError(err, "migrate")
+
+	// Cleanup
+	t.Cleanup(func() {
+		// Use require during setup, but prefer t.Errorf (non-fatal) in cleanup because if cleanup
+		// fails, the test has already finished running and failure reporting can be less clear
+		if err := adminDB.Exec(fmt.Sprintf(`DROP SCHEMA "%s" CASCADE`, schema)).Error; err != nil {
+			t.Errorf("drop schema: %v", err)
+		}
+		//adminDB.Exec(fmt.Sprintf(`DROP SCHEMA "%s" CASCADE`, schema))
+	})
+
+	return db
+}
