4ca4955a8cbbbc75a828737a5cabdbd6fa416c32
acholyn <a.ho-lyn@ucl.ac.uk>
2026-03-27 15:39:50 +0000

    attempting page change stuff

--- web/src/components/studies/StudyCardsList.tsx
+++ web/src/components/studies/StudyCardsList.tsx
@@ -6,6 +6,7 @@ import Button from "@/components/ui/Button";
 import styles from "./StudyCardsList.module.css";
 
 import dynamic from "next/dynamic";
+import { useState } from "react";
 const PaginationControls = dynamic(() => import("uikit-react-public").then((mod) => mod.Pagination.Controls), {
   ssr: false,
 });
@@ -31,11 +32,16 @@ export default function StudyCardsList(props: Props) {
   const { studies } = props;
   const router = useRouter();
 
+  const limit = 10;
+  const [offset, setOffset] = useState(0);
+
   return (
     <div className={styles["study-selection"]}>
       <div className={styles["studies-list"]}>
         {studies
+          .slice()
           .sort((a, b) => studySortOrder[a.approval_status] - studySortOrder[b.approval_status])
+          .slice(offset, offset + limit)
           .map((study) => (
             <div key={study.id} className={styles["study-card"]} data-cy="study-card">
               <div className={styles["status-indicator"]}>
@@ -63,9 +69,14 @@ export default function StudyCardsList(props: Props) {
             </div>
           ))}
       </div>
-      {studies.length > 1 && (
+      {studies.length > 10 && (
         <div className={styles["pagination-container"]}>
-          <Pagination total={studies.length} limit={1} offset={0}>
+          <Pagination
+            total={studies.length}
+            limit={limit}
+            offset={offset}
+            onPageChange={(newOffset) => setOffset(newOffset)}
+          >
             <PaginationControls />
             <PaginationInfo />
           </Pagination>
