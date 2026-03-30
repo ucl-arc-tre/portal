583628447e74df581a3b0b8a8175546d1f7ebd60
James Hughes <james.hughes@ucl.ac.uk>
2026-03-30 10:56:41 +0100

    refactoring studies components

--- web/src/components/studies/manage/StudyDetails.tsx
+++ web/src/components/studies/manage/StudyDetails.tsx
@@ -1,50 +1,53 @@
-import { Study, ApprovalStatus } from "@/openapi";
+import { Study } from "@/openapi";
 import Box from "../../ui/Box";
 import { Alert } from "../../shared/uikitExports";
 import StatusBadge from "../../ui/StatusBadge";
 import styles from "./StudyDetails.module.css";
 import InfoTooltip from "../../ui/InfoTooltip";
-import Loading from "../../ui/Loading";
 import { formatDate } from "../../shared/exports";
 
 type StudyOverviewProps = {
   study: Study;
   riskScore: number;
-  riskScoreLoading: boolean;
-  approvalStatus: ApprovalStatus | undefined;
-  feedback?: string;
 };
 
 export default function StudyDetails(props: StudyOverviewProps) {
-  const { study, approvalStatus, riskScore, riskScoreLoading, feedback } = props;
-
+  const { study, riskScore } = props;
   const standardRiskScoreStatement = "increases risk score by 5";
+
   return (
     <Box>
       <div className={styles["pre-description"]}>
         <span>
           Case ref: <span className={styles["grey-value"]}>{String(study.caseref).padStart(5, "0")}</span>
         </span>
+
         <span>
           Last updated: <span className={styles["grey-value"]}>{formatDate(study.updated_at)}</span>
         </span>
+
         {study.last_signoff && (
           <span>
             Last signed off: <span className={styles["grey-value"]}>{formatDate(study.last_signoff)}</span>
           </span>
         )}
+
         <span>
           Risk Score:
-          <span className={styles["risk-score"]}>{riskScoreLoading ? <Loading message={null} /> : riskScore}</span>
+          <span className={styles["risk-score"]}> {riskScore}</span>
         </span>
-        <StatusBadge status={approvalStatus} type="study" />
+
+        <StatusBadge status={study.approval_status} type="study" />
       </div>
+
       <h3 className={styles.description}>{study.description}</h3>
+
       <div>
         <dl className={styles.ownership}>
           <dd>
             Owner: <span className={styles["grey-value"]}>{study.owner_username}</span>
           </dd>
+
           <dd>
             Admins:
             {study.additional_study_admin_usernames.map((username) => (
@@ -53,6 +56,7 @@ export default function StudyDetails(props: StudyOverviewProps) {
               </li>
             ))}
           </dd>
+
           <dd>
             Data Controller:
             <span className={styles["grey-value"]}>{study.data_controller_organisation.toUpperCase()}</span>
@@ -66,18 +70,22 @@ export default function StudyDetails(props: StudyOverviewProps) {
           {study.involves_ucl_sponsorship && (
             <dd className={`${styles.badge} ${styles["badge-no-risk-associated"]}`}>UCL Sponsorship</dd>
           )}
+
           {study.involves_cag && (
             <dd className={`${styles.badge} ${styles["badge-risk-associated"]}`}>
               CAG approval
               <InfoTooltip text="increases risk score by 5" />
             </dd>
           )}
+
           {study.involves_ethics_approval && (
             <dd className={`${styles.badge} ${styles["badge-no-risk-associated"]}`}>Ethics approval</dd>
           )}
+
           {study.involves_hra_approval && (
             <dd className={`${styles.badge} ${styles["badge-no-risk-associated"]}`}>HRA approval</dd>
           )}
+
           {!study.involves_ucl_sponsorship &&
             !study.involves_cag &&
             !study.involves_ethics_approval &&
@@ -87,26 +95,31 @@ export default function StudyDetails(props: StudyOverviewProps) {
               </dd>
             )}
         </dl>
+
         <dl className={styles.grouping}>
           <dt>NHS</dt>
           {study.is_nhs_associated && (
             <dd className={`${styles.badge} ${styles["badge-no-risk-associated"]}`}>is NHS associated</dd>
           )}
+
           {study.involves_nhs_england && (
             <dd className={`${styles.badge} ${styles["badge-risk-associated"]}`}>
               NHS England involvement
               <InfoTooltip text={standardRiskScoreStatement} />
             </dd>
           )}
+
           {study.involves_mnca && (
             <dd className={`${styles.badge} ${styles["badge-no-risk-associated"]}`}>involves MNCA</dd>
           )}
+
           {study.requires_dspt && (
             <dd className={`${styles.badge} ${styles["badge-risk-associated"]}`}>
               requires DSPT
               <InfoTooltip text={standardRiskScoreStatement} />
             </dd>
           )}
+
           {!study.is_nhs_associated && !study.involves_nhs_england && !study.involves_mnca && !study.requires_dspt && (
             <dd>
               <em> No NHS information given</em>
@@ -115,36 +128,44 @@ export default function StudyDetails(props: StudyOverviewProps) {
         </dl>
         <dl className={styles.grouping}>
           <dt>Data</dt>
+
           {study.requires_dbs && (
             <dd className={`${styles.badge} ${styles["badge-risk-associated"]}`}>
               requires DBS
               <InfoTooltip text={standardRiskScoreStatement} />
             </dd>
           )}
+
           {study.is_data_protection_office_registered && (
             <dd className={`${styles.badge} ${styles["badge-no-risk-associated"]}`}>is registered with DPO</dd>
           )}
+
           {study.involves_third_party && (
             <dd className={`${styles.badge} ${styles["badge-risk-associated"]}`}>
               third party
               <InfoTooltip text="increases risk score by 5 if no mNCA" />
             </dd>
           )}
+
           {study.involves_external_users && (
             <dd className={`${styles.badge} ${styles["badge-no-risk-associated"]}`}>external users</dd>
           )}
+
           {study.involves_participant_consent && (
             <dd className={`${styles.badge} ${styles["badge-no-risk-associated"]}`}>participant consent</dd>
           )}
+
           {study.involves_indirect_data_collection && (
             <dd className={`${styles.badge} ${styles["badge-no-risk-associated"]}`}>indirect data collection</dd>
           )}
+
           {study.involves_data_processing_outside_eea && (
             <dd className={`${styles.badge} ${styles["badge-risk-associated"]}`}>
               data processing outside EEA
               <InfoTooltip text="increases risk score by 10" />
             </dd>
           )}
+
           {!study.requires_dbs &&
             !study.is_data_protection_office_registered &&
             !study.involves_third_party &&
@@ -158,11 +179,12 @@ export default function StudyDetails(props: StudyOverviewProps) {
             )}
         </dl>
 
-        {feedback && (
-          <Alert type={approvalStatus === "Approved" ? "info" : "warning"} className={styles["feedback-alert"]}>
+        {study.feedback && (
+          <Alert type={study.approval_status === "Approved" ? "info" : "warning"} className={styles["feedback-alert"]}>
             <h4>This study has been given the following feedback:</h4>
-            <p>{feedback}</p>
-            {approvalStatus !== "Approved" && (
+            <p>{study.feedback}</p>
+
+            {study.approval_status !== "Approved" && (
               <>
                 <hr></hr>
                 <small>
