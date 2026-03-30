c49c5106bde09c13d8caad80dcbb2aa3a0666178
acholyn <a.ho-lyn@ucl.ac.uk>
2026-03-30 10:49:30 +0100

    adding maxitems to queryparams

--- internal/handler/web/studies.go
+++ internal/handler/web/studies.go
@@ -79,6 +79,7 @@ func (h *Handler) studiesAll(params openapi.GetStudiesParams) ([]types.Study, er
 		CaseRef:        params.Caseref,
 		FuzzyTitle:     params.FuzzyTitle,
 		OwnerUsername:  params.OwnerUsername,
+		MaxItems:       params.MaxItems,
 	}
 	if queryIsCaseref(params.Query) {
 		caseref, err := strconv.Atoi(*params.Query)
