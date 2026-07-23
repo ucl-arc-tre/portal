5dab6f89d054106b1a5ba68bd53a344ff54c8185
t-young31 <tom.young.316@gmail.com>
2026-07-23 10:14:28 +0100

    fix tests

--- internal/controller/entra/invite_test.go
+++ internal/controller/entra/invite_test.go
@@ -12,17 +12,17 @@ func TestInviteEmailContent(t *testing.T) {
 	assert.NoError(t, err)
 
 	invite := Invite{Recipient: "a@example.com"}
-	assert.Equal(t, "You have been invited to the UCL ARC Services Portal.\nYou can login to the portal at https://portal@example.com.", inviteEmailContent(invite))
+	assert.Equal(t, "You have been invited to the UCL ARC Services Portal.\nYou can login to the portal at https://portal@example.com. The first thing you need to do is complete your profile.", inviteEmailContent(invite))
 
 	invite.Sponsor.Username = "bob@example.com"
-	assert.Equal(t, "You have been invited to the UCL ARC Services Portal by bob@example.com.\nYou can login to the portal at https://portal@example.com.", inviteEmailContent(invite))
+	assert.Equal(t, "You have been invited to the UCL ARC Services Portal by bob@example.com.\nYou can login to the portal at https://portal@example.com. The first thing you need to do is complete your profile.", inviteEmailContent(invite))
 
 	invite.Sponsor.ChosenName = "bob"
-	assert.Equal(t, "You have been invited to the UCL ARC Services Portal by bob.\nYou can login to the portal at https://portal@example.com.", inviteEmailContent(invite))
+	assert.Equal(t, "You have been invited to the UCL ARC Services Portal by bob.\nYou can login to the portal at https://portal@example.com. The first thing you need to do is complete your profile.", inviteEmailContent(invite))
 
 	invite.StudyName = new("example-study")
-	assert.Equal(t, "You have been invited to join the study 'example-study' by bob.\nYou can login to the portal at https://portal@example.com.", inviteEmailContent(invite))
+	assert.Equal(t, "You have been invited to join the study 'example-study' by bob.\nYou can login to the portal at https://portal@example.com. The first thing you need to do is complete your profile.", inviteEmailContent(invite))
 
 	invite.ProjectName = new("example-project")
-	assert.Equal(t, "You have been invited to join the project 'example-project' in the study 'example-study' by bob.\nYou can login to the portal at https://portal@example.com.", inviteEmailContent(invite))
+	assert.Equal(t, "You have been invited to join the project 'example-project' in the study 'example-study' by bob.\nYou can login to the portal at https://portal@example.com. The first thing you need to do is complete your profile.", inviteEmailContent(invite))
 }
