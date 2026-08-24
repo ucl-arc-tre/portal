601e72d90ab85a489bb07d46b132c714d93ee4ed
acholyn <a.ho-lyn@ucl.ac.uk>
2026-08-24 10:19:49 +0100

    rm unused export

--- web/src/components/shared/uikitExports.tsx
+++ web/src/components/shared/uikitExports.tsx
@@ -32,9 +32,6 @@ export const Label = dynamic(() => import("uikit-react-public").then((mod) => mo
 export const Alert = dynamic(() => import("uikit-react-public").then((mod) => mod.Alert), {
   ssr: false,
 });
-export const AlertTitle = dynamic(() => import("uikit-react-public").then((mod) => mod.Alert.Title), {
-  ssr: false,
-});
 export const AlertMessage = dynamic(() => import("uikit-react-public").then((mod) => mod.Alert.Message), {
   ssr: false,
 });
