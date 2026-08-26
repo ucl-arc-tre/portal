e69ad858222ec9822ad2a6fe337478f0411284a2
James Hughes <70143648+jhughes982@users.noreply.github.com>
2026-08-26 12:06:23 +0100

    now opening agreement links in tabs (#907)

--- web/src/components/ui/agreements/AgreementText.tsx
+++ web/src/components/ui/agreements/AgreementText.tsx
@@ -1,16 +1,22 @@
 import styles from "./AgreementText.module.css";
 import Markdown from "react-markdown";
+import { AnchorHTMLAttributes } from "react";
 
 type Props = {
   text: string;
 };
+
+// Override default Markdown anchor rendering to open links in a new tab
+function NewTabLink(props: AnchorHTMLAttributes<HTMLAnchorElement>) {
+  return <a {...props} target="_blank" rel="noopener noreferrer" />;
+}
 
 // a generic component to display different agreement texts
 export default function AgreementText(props: Props) {
   return (
     <section className={styles["agreement-text-container"]}>
       <div className={styles["agreement-text-content"]}>
-        <Markdown>{props.text}</Markdown>
+        <Markdown components={{ a: NewTabLink }}>{props.text}</Markdown>
       </div>
     </section>
   );
