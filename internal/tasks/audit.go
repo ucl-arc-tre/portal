4b3b7b42fde890967c4ec16d489043aa2634bdcb
t-young31 <tom.young.316@gmail.com>
2026-09-15 11:34:27 +0100

    refactor

--- internal/tasks/audit.go
+++ internal/tasks/audit.go
@@ -20,45 +20,50 @@ func (m *Manager) uploadAuditLog() error {
 	}
 
 	events := []types.AuditEvent{}
-	result := m.db.Preload("User").FindInBatches(&events, maxAuditEventBatchSize, func(_ *gorm.DB, batchNumber int) error {
-		errChan := make(chan error, 1)
-		pipeReader, pipeWriter := io.Pipe()
-		defer func() {
-			if err := pipeReader.Close(); err != nil {
-				log.Err(err).Msg("Failed to close pipe reader")
-			}
-		}()
-		batch := s3audit.AuditBatch{
-			BatchNumber: batchNumber,
-			Body:        pipeReader,
-		}
-
-		go func() { // async add events to data stream
-			defer func() {
-				if err := pipeWriter.Close(); err != nil {
-					log.Err(err).Msg("Failed to close pipe writer")
-				}
-			}()
-
-			for _, event := range events {
-				eventData, err := event.Marshal()
-				if err != nil {
-					errChan <- err
-					return
-				}
-				eventData = append(eventData, '\n')
-				if _, err = pipeWriter.Write(eventData); err != nil {
-					errChan <- err
-					return
-				}
-			}
-			errChan <- nil
-		}()
-		if err := m.s3Audit.Upload(batch); err != nil {
-			return err
-		}
-		return <-errChan
-	})
+	result := m.db.Preload("User").
+		FindInBatches(&events, maxAuditEventBatchSize, func(_ *gorm.DB, batchNumber int) error {
+			return m.uploadAuditLogBatch(events, batchNumber)
+		})
 
 	return types.NewErrFromGorm(result.Error, "failed to find audit events in batches")
+}
+
+func (m *Manager) uploadAuditLogBatch(events []types.AuditEvent, batchNumber int) error {
+	errChan := make(chan error, 1)
+	pipeReader, pipeWriter := io.Pipe()
+	defer func() {
+		if err := pipeReader.Close(); err != nil {
+			log.Err(err).Msg("Failed to close pipe reader")
+		}
+	}()
+	batch := s3audit.AuditBatch{
+		BatchNumber: batchNumber,
+		Body:        pipeReader,
+	}
+
+	go func() { // async add events to data stream
+		defer func() {
+			if err := pipeWriter.Close(); err != nil {
+				log.Err(err).Msg("Failed to close pipe writer")
+			}
+		}()
+
+		for _, event := range events {
+			eventData, err := event.Marshal()
+			if err != nil {
+				errChan <- err
+				return
+			}
+			eventData = append(eventData, '\n')
+			if _, err = pipeWriter.Write(eventData); err != nil {
+				errChan <- err
+				return
+			}
+		}
+		errChan <- nil
+	}()
+	if err := m.s3Audit.Upload(batch); err != nil {
+		return err
+	}
+	return <-errChan
 }
