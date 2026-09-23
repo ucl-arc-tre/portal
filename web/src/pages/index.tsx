095bc4808b4c0d3b9ba5aff3a3a1d5574822a587
t-young31 <tom.young.316@gmail.com>
2026-09-23 12:21:03 +0100

    add homepage text

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -2,8 +2,24 @@ import styles from "./index.module.css";
 import MetaHead from "@/components/meta/Head";
 import Notifications from "@/components/index/Notifications";
 import Title from "@/components/ui/Title";
+import { useAuth } from "@/hooks/useAuth";
+import LoginFallback from "@/components/ui/LoginFallback";
+
+const nonIAODescription = `You can:\n
+  • Register and manage your studies
+  • Maintain information asset registers for research data
+  • Upload agreements and training records
+  • Manage study team members and access rights
+  • Request access to research systems and services
+`;
 
 export default function Index() {
+  const { authInProgress, isAuthed, userData, isIAO } = useAuth();
+
+  if (authInProgress) return null;
+
+  if (!isAuthed || !userData) return <LoginFallback />;
+
   return (
     <>
       <MetaHead title="ARC Services Portal | UCL" description="ARC Services Portal homepage" />
@@ -12,7 +28,7 @@ export default function Index() {
         <Title
           text={"Welcome to the ARC Services Portal"}
           centered
-          description={"This portal allows UCL researchers to manage ARC services and tasks"}
+          description={`Use this portal to register studies, manage data assets and contracts,\nand administer access to UCL research services. ${!isIAO && nonIAODescription}`}
         />
       </div>
 
