32c690caf49212d318aeaf4ec2146c484d097926
t-young31 <tom.young.316@gmail.com>
2026-09-23 12:30:30 +0100

    non conditional

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -2,10 +2,8 @@ import styles from "./index.module.css";
 import MetaHead from "@/components/meta/Head";
 import Notifications from "@/components/index/Notifications";
 import Title from "@/components/ui/Title";
-import { useAuth } from "@/hooks/useAuth";
-import LoginFallback from "@/components/ui/LoginFallback";
 
-const nonIAODescription = `You can:\n
+const description = `Use this portal to register studies, manage data assets and contracts,\nand administer access to UCL research services. You can:\n
   • Register and manage your studies
   • Maintain information asset registers for research data
   • Upload agreements and training records
@@ -14,22 +12,12 @@ const nonIAODescription = `You can:\n
 `;
 
 export default function Index() {
-  const { authInProgress, isAuthed, userData, isIAO } = useAuth();
-
-  if (authInProgress) return null;
-
-  if (!isAuthed || !userData) return <LoginFallback />;
-
   return (
     <>
       <MetaHead title="ARC Services Portal | UCL" description="ARC Services Portal homepage" />
 
       <div className={styles.title}>
-        <Title
-          text={"Welcome to the ARC Services Portal"}
-          centered
-          description={`Use this portal to register studies, manage data assets and contracts,\nand administer access to UCL research services. ${!isIAO && nonIAODescription}`}
-        />
+        <Title text={"Welcome to the ARC Services Portal"} centered description={description} />
       </div>
 
       <Notifications />
